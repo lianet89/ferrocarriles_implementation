c4597039f7ffb54a44bca5b4a2f5b3eafbc8128e
Lianet Cabrera González <lianetcabreragonzalez@gmail.com>
2023-06-12 02:35:42 -0400

    Optimizando implementación
    Optimizando la implementación de los métodos para adicionar un equipo de tracción y adicionar viaje. Optimizando la implementación de la clase main y los métodos de la clase Estación de trenes

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,14 @@ public class Main {
         ArrayList <Viaje> viajes = new ArrayList<Viaje>();
         EstacionDeTrenes estacion = new EstacionDeTrenes(viajes, lineaPrincipal, lineaSecundaria);
 
-        Itinerario itinerario1 = new Itinerario(1, 300, 20,"La Habana", "Sancti Spiritus"); //No existen equipos disponibles
-        Itinerario itinerario2 = new Itinerario(2, 500, 2,"Matanzas", "Las Tunas"); //Coche motor con 555km recorridos
-        Itinerario itinerario3 = new Itinerario(3, 850, 10,"Pinar del Río", "Camagûey"); //Locomotora con 940km recorridos
-        Itinerario itinerario4 = new Itinerario(4, 148, 12,"Pinar del Río", "La Habana"); //No existen equipos disponibles
+        Itinerario itinerario1 = new Itinerario(1, 300, 20,"La Habana", "Sancti Spiritus");
+        Itinerario itinerario2 = new Itinerario(2, 500, 2,"Matanzas", "Las Tunas");
+        Itinerario itinerario3 = new Itinerario(3, 850, 10,"Pinar del Río", "Camagûey");
+        Itinerario itinerario4 = new Itinerario(4, 148, 12,"Pinar del Río", "La Habana");
 
         EquipoDeTraccion locomotora1 = new Locomotora(1111, 940,3500, "SD50");
         EquipoDeTraccion locomotora2 = new Locomotora(2222, 600,4500, "DF76");
-        EquipoDeTraccion locomotora3 = new Locomotora(3333, 1030, 2000, "SD50");
+        EquipoDeTraccion locomotora3 = new Locomotora(3333, 1030, 2000, "SD00");
         EquipoDeTraccion cocheMotor1 = new CocheMotor(4444, 2500, 4000, true);
         EquipoDeTraccion cocheMotor2 = new CocheMotor(5555, 555, 5300, false);
         EquipoDeTraccion cocheMotor3 = new CocheMotor(6666, 4850,3000, true);
@@ -30,23 +30,13 @@ public class Main {
         lineaPrincipal.add(cocheMotor3);
 
         try {
-            System.out.println("La línea principal tiene "+ lineaPrincipal.size()+" equipos de tracción.\n");
-            estacion.adicionarEquipoDeTraccion(locomotora3);   //Agregar nuevo equipo de tracción a la estación
-            System.out.println("Después de adicionarEquipoDeTracción(locomotora3), la línea principal tiene "+ lineaPrincipal.size()+" equipos de tracción.\n");
+            estacion.adicionarEquipoDeTraccion(locomotora3);
             estacion.adicionarViaje(itinerario1);
-            System.out.println("Después de adicionarViaje(itinerario1), la línea principal tiene "+ lineaPrincipal.size()+" equipos de tracción.\n");
             estacion.adicionarViaje(itinerario2);
-            System.out.println("Después de adicionarViaje(itinerario2), la línea principal tiene "+ lineaPrincipal.size()+" equipos de tracción.\n");
             estacion.adicionarViaje(itinerario3);
-            System.out.println("Después de adicionarViaje(itinerario3), la línea principal tiene "+ lineaPrincipal.size()+" equipos de tracción.\n");
             estacion.adicionarViaje(itinerario4);
-            System.out.println("Después de adicionarViaje(itinerario4), la línea principal tiene "+ lineaPrincipal.size()+" equipos de tracción.\n");
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
-
-
-
-
     }
 }
--- src/controller/EstacionDeTrenes.java
+++ src/controller/EstacionDeTrenes.java
@@ -20,30 +20,33 @@ public class EstacionDeTrenes {
     }
     public void adicionarEquipoDeTraccion(EquipoDeTraccion equipo) throws Exception {
         lineaPrincipal.add(equipo);
+        System.out.println("Se ha adicionado satisfactoriamente el equipo de tracción con número de identificación "+equipo.getNumeroIdentificacion()+".\n");
     }
     public void adicionarViaje(Itinerario itinerario) throws Exception{
-        boolean equipoDisponble=false;
-        // Ordenar la lista lineaPrincipal en orden asc por el atributo kilometrajeRecorrido
+        boolean equipoDisponible =false;
+        // Ordenar la lista lineaPrincipal en orden ascendente por el atributo kilometrajeRecorrido
         Comparator<EquipoDeTraccion> porKilometraje = Comparator.comparing(EquipoDeTraccion::getKilometrajeRecorrido);
         Collections.sort(lineaPrincipal, porKilometraje);
 
         /*Recorrer la lista lineaPrincipal que contiene los equipos libres (estacionados, que no están asignados a viajes).
-         Verificar que el equipo es capaz de tirar la cantidad de coches que exige el itinerario (mediante sentencia if)
-         Si cumple con el requisito, crear el viaje con ese equipo, actualizar las listas y dejar de iterar
+         Verificar si el equipo es capaz de tirar la cantidad de coches que exige el itinerario (mediante sentencia if)
+         Si es capaz, crear el viaje con ese equipo e itinerarios e insertarlo en la lista de viajes.
+         Sacar el equipo de la línea principal y ponerlo en la línea secundaria.
+         Dejar de iterar.
         */
         for (EquipoDeTraccion equipo:lineaPrincipal) {
             if (equipo.cantidadMaxCoches()==itinerario.getCantidadVagones()){
-                equipoDisponble = true;
-                Viaje nuevoViaje = new Viaje(equipo, itinerario);       //crear el viaje
-                viajes.add(nuevoViaje);             //insertar el viaje en la lista de viajes
-                System.out.println("El equipo adecuado para el itinerario número "+itinerario.getNumeroIdentificacion()+" ha recorrido: "+equipo.getKilometrajeRecorrido()+" kilómetros");
-                lineaSecundaria.add(equipo);       //adicionar el equipo a la linea secundaria
-                lineaPrincipal.remove(equipo);     //eliminar el equipo de la linea principal
+                equipoDisponible = true;
+                Viaje nuevoViaje = new Viaje(equipo, itinerario);
+                viajes.add(nuevoViaje);
+                System.out.println("El equipo adecuado para el itinerario número "+itinerario.getNumeroIdentificacion()+" ha recorrido "+equipo.getKilometrajeRecorrido()+" kilómetros.");
+                lineaSecundaria.add(equipo);
+                lineaPrincipal.remove(equipo);
                 break;
             }
         }
-        if (equipoDisponble==false){
-            System.out.println("No existen equipos disponibles para el itinerario número "+ itinerario.getNumeroIdentificacion());
+        if (equipoDisponible ==false){
+            System.out.println("No existen equipos disponibles para el itinerario número "+ itinerario.getNumeroIdentificacion()+".");
         }
 
     }
